83584c73267afdf21064a3b52f0b30d975d6a956
Noah TerBest <noahterbest@gmail.com>
2024-02-18 21:30:33 -0700

    Setup

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Variables
+        int randomNumber1, randomNumber2, randomNumber3;
+        int userGuess1, userGuess2, userGuess3;
+
+        // Welcome to the Lottery Game!
+        System.out.println("Hello! This program will generate 3 random numbers between 1 and 10");
+        System.out.println("Your job will be to guess the numbers, lets begin!");
+        System.out.println(" ");
+
+        // Create a scanner object
+        Scanner input = new Scanner(System.in);
+
+
+    }
+
+}
